5471603838f1aa975684f974cbd702f91bc6cc3e
jameswu05 <jameswu382@gmail.com>
2025-08-07 23:26:46 -0400

    removed chatGPT specifications

--- bayes_opt/GaussianProcess/kernels.py
+++ bayes_opt/GaussianProcess/kernels.py
@@ -6,18 +6,6 @@ def RBFkernel(X1, X2, l=1.0, sigma_f=1.0):
     return (sigma_f**2) * np.exp(-diff / (2*l**2))
 
 def GaussianKernel(X1, X2, alpha0=1.0, alpha=None):
-    """
-    Computes the Gaussian (RBF) kernel matrix between X1 and X2.
-    
-    Parameters:
-    - X1: shape (n1, d)
-    - X2: shape (n2, d)
-    - alpha0: scaling factor (alpha_0)
-    - alpha: list or array of length d for anisotropic scaling (alpha_1 to alpha_d). If None, isotropic with 1.0
-    
-    Returns:
-    - K: kernel matrix of shape (n1, n2)
-    """
     X1 = np.atleast_2d(X1)
     X2 = np.atleast_2d(X2)
     
@@ -30,19 +18,6 @@ def GaussianKernel(X1, X2, alpha0=1.0, alpha=None):
     return alpha0 * np.exp(-dists)
 
 def MaternKernel(X1, X2, alpha0=1.0, nu=1.5, length_scale=1.0):
-    """
-    Computes the Matern kernel between points in X1 and X2.
-    
-    Parameters:
-    - X1: shape (n1, d)
-    - X2: shape (n2, d)
-    - alpha0: scaling constant (α0)
-    - nu: smoothness parameter (commonly 0.5, 1.5, 2.5, ∞)
-    - length_scale: scalar or array of length d
-    
-    Returns:
-    - Kernel matrix of shape (n1, n2)
-    """
     X1 = np.atleast_2d(X1)
     X2 = np.atleast_2d(X2)
 
