fc4ecab78737ff68055e9a8d3562fe500c5c0205
jameswu05 <jameswu382@gmail.com>
2025-08-08 16:33:26 -0400

    began implementing expected improvement acquisition function

---
+++ bayes_opt/AcquisitionFunctions/expectedImprovement.py
@@ -0,0 +1,12 @@
+import numpy as np
+from scipy.stats import norm
+
+def expectedImprovement(X, GPPosteriorMean, GPPosteriorSTD, f_best):
+    mu = np.array(GPPosteriorMean)
+    sigma = np.array(GPPosteriorSTD)
+    sigma = np.maximum(sigma, 1e-10)
+    delta = f_best - mu
+    Z = delta / sigma
+    e_i = delta * norm.cdf(Z) + sigma * norm.pdf(Z)
+    e_i = max(e_i, 0.0)
+    return max(e_i)
